98409adf8bca634fa98b60498931513a19bc58b7
pine.jin <pine.jin@SH-DEV-8049>
2017-03-08 14:30:21 +0800

    添加项目文件。

---
+++ devmgr/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(devmgr.Startup))]
+namespace devmgr
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
