dd47b5681c7e984fb4e13d4a560619b84153a8fb
靳松 <jinsongdhu@outlook.com>
2017-04-23 23:51:43 +0800

    d

--- devmgr/Controllers/MyManagerController.cs
+++ devmgr/Controllers/MyManagerController.cs
@@ -1,30 +1,100 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
+using devmgr.Models;
 
 namespace devmgr.Controllers
 {
     public class MyManagerController : Controller
     {
+        private Model1 db = new Model1();
+
         // GET: MyManager
-        public ActionResult Index()
+        public ActionResult Index(int? id)
         {
-            return View();
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            SYS_USER sYS_USER = db.SYS_USER.Find(id);
+            if (sYS_USER == null)
+            {
+                return HttpNotFound();
+            }
+            return View(sYS_USER);
         }
 
         //GET
-        public ActionResult Edit()
+        public ActionResult Edit(int? id)
         {
-            return View();
-        }
+            List<SYS_DEPART> categories = SYS_DEPART.GETALL();
+            ViewData["Categories"] = new SelectList(categories, "id", "name");
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            SYS_USER sYS_USER = db.SYS_USER.Find(id);
+            if (sYS_USER == null)
+            {
+                return HttpNotFound();
+            }
 
-        //GET
-        public ActionResult changepasswd()
+            return View(sYS_USER);
+        }
+        public ActionResult changepasswd(int? id)
         {
+            List<SYS_DEPART> categories = SYS_DEPART.GETALL();
+            ViewData["Categories"] = new SelectList(categories, "id", "name");
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            SYS_USER sYS_USER = db.SYS_USER.Find(id);
+            if (sYS_USER == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(sYS_USER);
             return View();
         }
+        /*
+         * public ActionResult Edit(int? id)
+            {
+                List<SYS_DEPART> categories = SYS_DEPART.GETALL();
+                ViewData["Categories"] = new SelectList(categories, "id", "name");
+                if (id == null)
+                {
+                    return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+                }
+                SYS_USER sYS_USER = db.SYS_USER.Find(id);
+                if (sYS_USER == null)
+                {
+                    return HttpNotFound();
+                }
+
+                return View(sYS_USER);
+            }
+          public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            SYS_USER sYS_USER = db.SYS_USER.Find(id);
+            if (sYS_USER == null)
+            {
+                return HttpNotFound();
+            }
+            return View(sYS_USER);
+        }
+             */
+        //GET
+
     }
-}
 }
