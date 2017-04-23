75eb6d7972a305b3e17ad03ba060dca734ec77ce
靳松 <jinsongdhu@outlook.com>
2017-04-23 23:15:18 +0800

    2

---
+++ devmgr/Controllers/MyManagerController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace devmgr.Controllers
+{
+    public class MyManagerController : Controller
+    {
+        // GET: MyManager
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //GET
+        public ActionResult Edit()
+        {
+            return View();
+        }
+
+        //GET
+        public ActionResult changepasswd()
+        {
+            return View();
+        }
+    }
+}
+}
